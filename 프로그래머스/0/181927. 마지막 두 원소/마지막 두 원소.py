58d65d614cd610ea2d6136305b9eeef24912650d
202021013 <88577960+202021013@users.noreply.github.com>
2024-01-19 14:02:14 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -3,5 +3,5 @@ def solution(num_list):
     if answer[-1] > answer[-2]:
         answer.append(answer[-1] - answer[-2])
     else:
-        answer += [answer[-1] * 2]
+        answer.append(answer[-1] * 2)
     return answer
