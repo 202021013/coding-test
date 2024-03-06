ad5534e64dd24a978854fec88165550c8068a0bf
jiyeon <88577960+202021013@users.noreply.github.com>
2024-03-06 16:58:45 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -11,11 +11,11 @@ def solution(genres, plays):
         genre_dict[genre].append((i, play_count))
 
     # 재생 횟수가 많은 장르 순서대로 정렬
-    sorted_genres = sorted(genre_dict.keys(), key=lambda x: sum(y[1] for y in genre_dict[x]), reverse=True)
+    sorted_genres = sorted(genre_dict.keys(), key=lambda genre: sum(song[1] for song in genre_dict[genre]), reverse=True)
 
     # 각 장르별로 노래를 재생 횟수 내림차순, 고유 번호 오름차순으로 정렬하여 최대 2개씩 answer에 추가
     for genre in sorted_genres:
-        songs = sorted(genre_dict[genre], key=lambda x: (x[1], -x[0]), reverse=True)
+        songs = sorted(genre_dict[genre], key=lambda song: (song[1], -song[0]), reverse=True)
         answer.extend([song[0] for song in songs[:2]])
 
     return answer
