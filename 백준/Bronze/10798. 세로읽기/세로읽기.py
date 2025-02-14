e10dc60867927912c608524e4f9d2255cf1fe6d8
jiyeon <88577960+202021013@users.noreply.github.com>
2025-02-14 22:42:28 +0900

    [Bronze I] Title: 세로읽기, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -0,0 +1,9 @@
+words = [input().strip() for _ in range(5)]
+
+result = ""
+for j in range(15):
+    for i in range(5):
+        if j < len(words[i]):
+            result += words[i][j]
+
+print(result)
