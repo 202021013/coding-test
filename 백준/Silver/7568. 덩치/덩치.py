7245aee1fed6d5fa250e382740b663ce5e402fa5
jiyeon <88577960+202021013@users.noreply.github.com>
2025-03-09 20:17:57 +0900

    [Silver V] Title: 덩치, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,15 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+people = [tuple(map(int, input().split())) for _ in range(N)]
+
+ranks = []
+for i in range(N):
+    rank = 1
+    for j in range(N):
+        if i != j:
+            if people[i][0] < people[j][0] and people[i][1] < people[j][1]:
+                rank += 1
+    ranks.append(rank)
+print(*ranks)
