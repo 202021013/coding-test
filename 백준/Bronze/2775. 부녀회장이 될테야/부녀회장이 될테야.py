364230bcf31a714416948a38691da5c12a499b11
jiyeon <88577960+202021013@users.noreply.github.com>
2025-03-12 20:50:11 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -1,10 +1,10 @@
 # 1. 0층 초기화, 누적 계산하는 함수 만들기
-def apartment_residents_optimized(k, n):
-    residents = list(range(1, n+1))  # 0층 초기화
+def apartment_residents(k, n):
+    residents = list(range(1, n+1))
     
     for _ in range(k):
         for i in range(1, n):
-            residents[i] += residents[i-1]  # 이전 값 누적 합 계산
+            residents[i] += residents[i-1]
     
     return residents[-1]
 
@@ -14,4 +14,4 @@ for _ in range(T):
     k = int(input())
     n = int(input())
     # 3. 입력받은 k, n 값을 함수에 적용하여 결과 출력하기
-    print(apartment_residents_optimized(k, n))
+    print(apartment_residents(k, n))
